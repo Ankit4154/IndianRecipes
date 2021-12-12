d11efe57fd671278adb1ce818b89299bec38b8a3
Ankit Singh <ankitsingh4154@gmail.com>
2021-12-12 23:16:58 +0530

    Modified controller to call library-event topic producer

--- src/main/java/com/recipe/indianrecipe/controller/LibraryEventsController.java
+++ src/main/java/com/recipe/indianrecipe/controller/LibraryEventsController.java
@@ -1,21 +1,28 @@
 package com.recipe.indianrecipe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.recipe.indianrecipe.model.LibraryEvent;
+import com.recipe.indianrecipe.producer.LibraryEventProducer;
 
 @RestController
 public class LibraryEventsController {
-		
+	
+	@Autowired
+	LibraryEventProducer libraryEventProducer;
 	
 	@PostMapping("/v1/libraryevent")
-	public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+	public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException{
 		
 		// invoke kafka producer
+		libraryEventProducer.sendLibraryEvent(libraryEvent);
+		
 		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 		
 		
