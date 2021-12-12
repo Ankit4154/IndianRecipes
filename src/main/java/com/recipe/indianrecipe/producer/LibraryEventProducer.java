2086f0b1abfe5002cfeefb70d02411272f216398
Ankit Singh <ankitsingh4154@gmail.com>
2021-12-12 23:11:09 +0530

    Added kafka template to send the message to default topic : library-events

---
+++ src/main/java/com/recipe/indianrecipe/producer/LibraryEventProducer.java
@@ -0,0 +1,61 @@
+package com.recipe.indianrecipe.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.recipe.indianrecipe.model.LibraryEvent;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class LibraryEventProducer {
+	
+	@Autowired
+	KafkaTemplate<Integer, String> kafkaTemplates;
+	
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+		Integer key = libraryEvent.getLibraryEvenId();
+		String value = objectMapper.writeValueAsString(libraryEvent);
+		ListenableFuture<SendResult<Integer,String>> listenableFuture = kafkaTemplates.sendDefault(key, value);
+		listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer,String>>(){
+
+			@Override
+			public void onSuccess(SendResult<Integer, String> result) {
+				handleSuccess(key, value, result);
+				
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				handleFailure(key, value, ex);
+				
+			}
+			
+		});
+	}
+
+	protected void handleFailure(Integer key, String value, Throwable ex) {
+		log.error("Error sending the message and the exception is {}",ex.getMessage());
+		try {
+			throw ex;
+		}catch(Throwable throwable) {
+			log.error("Error is OnFailure : {}",throwable.getMessage());
+		}
+	}
+
+	protected void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+		log.info("Message sent successfully for the key : {} and the value is {} "
+				+ ", partition is {}",key,value,result.getRecordMetadata().partition());
+		
+	}
+}
